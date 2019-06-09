e9ed2c70ab00f6a9eb08a3a0101030a69015be70
Keerthi Jagalur Mutt <keerthijm@gmail.com>
2019-06-09 11:50:15 +0100

    Score increased by 400 if same card appears twise when there is no Joker
    Score increased by 400 if same card appears twise when there is no Joker

--- CGBusinessLayer/OperateCard.cs
+++ CGBusinessLayer/OperateCard.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BusinessLayer
@@ -8,14 +8,38 @@ namespace BusinessLayer
         public int CalculateScore(List<Card> cards)
         {
             var score = 0;
+            List<int> tempCardValues = new List<int>();
             var countJoker = cards.Count(i => i.IsJoker);
+
             foreach (var card in cards)
             {
                 var temp = card.GetValue();
-                if (!card.IsJoker) score += temp;
+                if (!card.IsJoker)
+                {
+                    score += temp;
+                    tempCardValues.Add((int)card.CardNumber);
+                }
             }
 
             for (var i = 1; i <= countJoker; i++) score *= 2;
+
+            score = ScoreBasedonSameCardDuplicateAppearence(countJoker, score, tempCardValues);
+
+            return score;
+        }
+
+        private static int ScoreBasedonSameCardDuplicateAppearence(int jockerCount, int score, List<int> tempCardValues)
+        {
+            // Check if temp has same values more then twice
+            if (jockerCount == 0)
+            {
+                var duplicateExists = tempCardValues.GroupBy(n => n).Any(g => g.Count() > 1);
+                if (duplicateExists)
+                {
+                    // Duplicates exist
+                    score += 400;
+                }
+            }
 
             return score;
         }
